9777924298319ae32d582de85eb1722f47dcaadf
Thomas <77514919+TB516@users.noreply.github.com>
2026-06-12 07:27:22 +0000

    helium-browser-linux 0.13.3.1

--- Casks/helium-browser-linux.rb
+++ Casks/helium-browser-linux.rb
@@ -2,9 +2,9 @@ cask "helium-browser-linux" do
   arch arm: "arm64", intel: "x86_64"
   os linux: "linux"
 
-  version "0.13.2.1"
-  sha256 arm64_linux:  "397450a452f7ac657ed24a19e199988a862e088c40dbf72e3a0ecfe3c1a65a52",
-         x86_64_linux: "3b21c86dd118041c8568435ad160fb5a853380baf211a4f8eeef05913ece63e7"
+  version "0.13.3.1"
+  sha256 arm64_linux:  "0d2949c734401614e44f2605c94af2a5ffa5794f928a9da990bb4ebb221dbb35",
+         x86_64_linux: "47f706c96b81acb78586e72ba6446940df64fcc58ddc9967c12b9f12ca959266"
 
   url "https://github.com/imputnet/helium-linux/releases/download/#{version}/helium-#{version}-#{arch}_linux.tar.xz"
   name "Helium"
