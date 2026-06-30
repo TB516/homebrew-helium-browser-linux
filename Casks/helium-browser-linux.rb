eab69477224d5be00eb4cc230b4c6ad1ccc3555b
Thomas <77514919+TB516@users.noreply.github.com>
2026-06-29 22:43:28 -0400

    Add on_linux block around SHA

--- Casks/helium-browser-linux.rb
+++ Casks/helium-browser-linux.rb
@@ -3,8 +3,11 @@ cask "helium-browser-linux" do
   os linux: "linux"
 
   version "0.13.4.1"
-  sha256 arm64_linux:  "c661c3eb9254206e2fb95fc86ca203be8588a8c7b5e42e6a079fa37075c3be49",
-         x86_64_linux: "aedfffc1c0271fb9f5a25fcf7cfdfb6b11e92142ab59749037a4a2b06b44ee1c"
+
+  on_linux do
+    sha256 arm64_linux:  "c661c3eb9254206e2fb95fc86ca203be8588a8c7b5e42e6a079fa37075c3be49",
+           x86_64_linux: "aedfffc1c0271fb9f5a25fcf7cfdfb6b11e92142ab59749037a4a2b06b44ee1c"
+  end
 
   url "https://github.com/imputnet/helium-linux/releases/download/#{version}/helium-#{version}-#{arch}_linux.tar.xz"
   name "Helium"
